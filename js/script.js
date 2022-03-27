1b7885182284ad12d2c6421468e4348882353e16
Randal Michel <randalmichel@gmail.com>
2022-03-27 00:11:34 -0400

    added typing animation

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*============== typing animation */
+const typingClass = document.querySelector(".typing");
+
+const words = ["Web Developer", "Computer Science Student", "Hungry"];
+
+let count = 0;
+let letterCount = 0;
+
+function type() {
+  if (count === words.length) {
+    count = 0;
+  }
+  let currentWords = words[count];
+  let letters = currentWords.slice(0, ++letterCount);
+  typingClass.textContent = letters;
+  if (letters.length === currentWords.length) {
+    letterCount = 0;
+    count++;
+  }
+  setTimeout(type, 400);
+}
+
+type();
