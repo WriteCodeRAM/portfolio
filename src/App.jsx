5612348cbd9cf0630773a06fe18c24df1b59a7ff
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-08-30 14:17:02 -0400

    initial commit and static TV background added

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import p5 from 'p5';
+import './App.css';
+
+const App = () => {
+  
+  useEffect(() => {
+    
+    const background = new p5(p => {
+      p.setup = () => {
+        p.createCanvas(window.innerWidth, window.innerHeight);
+      };
+
+      p.draw = () => {
+        p.background(150);
+        p.noStroke();
+        for (let i = 0; i < 14000; i++) {
+          p.rect(p.random(p.width), p.random(p.height), 2, 2);
+        }
+      };
+    });
+
+   
+    return () => {
+      background.remove();
+    };
+  }, []);
+
+  return <div className="wallpaper"></div>;
+};
+
+export default App;
